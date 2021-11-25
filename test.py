8f327760a9e7d5f5bb1fa84266ef2709799a8f2c
Darren Thompson <darren.thompson200@gmail.com>
2021-11-25 01:35:19 -0500

    Remove test file

--- test.py
+++
@@ -1,7 +0,0 @@
-from ranking_model import MovieRankingModel
-
-model = MovieRankingModel()
-
-for row in model.get_top_results('toy', 'Genre_Children', 10):
-  movie_data = row.get_info()
-  print(f"{movie_data['title']} ({movie_data['year']}) -- Rating: {movie_data['rating']} -- Score: {movie_data['score']}")
